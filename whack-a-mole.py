0c148d38c40814a710e5c67bface833fd4c81033
Srivarun09 <168688130+Srivarun09@users.noreply.github.com>
2026-09-09 22:29:53 +1200

    Add files via upload

--- whack-a-mole.py
+++ whack-a-mole.py
@@ -10,12 +10,16 @@ class MyFirstWindow(QMainWindow):
         self.setFixedSize(400, 400)
 
         grid_box = QGridLayout()
+        self.buttons = []
         for row in range(4):
+            row_buttons = []
             for column in range(4):
                 button = QPushButton("")
                 grid_box.addWidget(button, row, column)
+                row_buttons.append(button)
+                button.clicked.connect(lambda clicked, r=row, c=column: self.button_clicked(r, c))
+                self.buttons.append(row_buttons)
 
-        
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         central_widget.setLayout(grid_box)
@@ -24,3 +28,4 @@ app = QApplication(sys.argv)
 window = MyFirstWindow()
 window.show()
 sys.exit(app.exec())
+        
