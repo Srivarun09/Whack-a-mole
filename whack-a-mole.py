3dc821622c0fd248c7d0d7a94e43ff6a1fa0d2a6
Srivarun09 <168688130+Srivarun09@users.noreply.github.com>
2026-09-10 20:51:27 +1200

    Add files via upload

--- whack-a-mole.py
+++ whack-a-mole.py
@@ -1,4 +1,5 @@
 import sys
+import random 
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout, QWidget
 
 
@@ -9,23 +10,45 @@ class MyFirstWindow(QMainWindow):
         self.setWindowTitle("Whack-a-mole")
         self.setFixedSize(400, 400)
 
-        grid_box = QGridLayout()
+        self.grid_box = QGridLayout()
         self.buttons = []
-        for row in range(4):
-            row_buttons = []
-            for column in range(4):
-                button = QPushButton("")
-                grid_box.addWidget(button, row, column)
-                row_buttons.append(button)
-                button.clicked.connect(lambda clicked, r=row, c=column: self.button_clicked(r, c))
-                self.buttons.append(row_buttons)
+
+        self.create_gridbox()
+        self.place_mole()
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-        central_widget.setLayout(grid_box)
+        central_widget.setLayout(self.grid_box)
+
+    def create_gridbox(self):
+        for row in range(4):
+            row_buttons = []
+
+            for column in range(4):
+                button = QPushButton("")
+                self.grid_box.addWidget(button, row, column)
+                row_buttons.append(button)
+
+                button.clicked.connect(lambda clicked, r=row, c=column: self.button_clicked(r, c))
+                
+            self.buttons.append(row_buttons)
+
+    def place_mole(self):
+        self.mole_row = random.randint(0, 3)
+        self.mole_col = random.randint(0, 3)
+
+        self.buttons[self.mole_row][self.mole_col].setText("MOLE")
+
+    def button_clicked(self, row, column):
+        if row == self.mole_row and column == self.mole_col:
+            print("Mole clicked!")
+
+            self.buttons[self.mole_row][self.mole_col].setText("")
+
+            self.place_mole()
+
 
 app = QApplication(sys.argv)
 window = MyFirstWindow()
 window.show()
 sys.exit(app.exec())
-        
